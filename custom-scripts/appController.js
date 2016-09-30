59067d00c1943d868517f2b59a6b91f6deb2903b
nikhilSangwan <nikhil.sangwan95@gmail.com>
2016-09-30 15:38:17 +0530

    put login button

--- custom-scripts/appController.js
+++ custom-scripts/appController.js
@@ -1,33 +1,11 @@
 angular.module('app')
 
 	.controller('home-content-ctrl', ['$scope', '$mdDialog', function($scope, $mdDialog){
-		console.log("enter");
-        $scope.popDialog = function(event) {
-           $mdDialog.show({
-              clickOutsideToClose: true,
-              scope: $scope,        
-              preserveScope: true,           
-              template: '<md-dialog aria-label="List dialog">' +
-                         '  <md-dialog-content>'+
-                         '    <md-list>'+
-                         '      <md-list-item ng-repeat="item in items">'+
-                         '       <p>* <b>{{item}}</b></p>' +
-                         '      '+
-                         '    </md-list-item></md-list>'+
-                         '  </md-dialog-content>' +
-                         '  <md-dialog-actions>' +
-                         '    <md-button ng-click="closeDialog()" class="md-primary">' +
-                         '      Close' +
-                         '    </md-button>' +
-                         '  </md-dialog-actions>' +
-                      '</md-dialog>',
-              controller: function DialogController($scope, $mdDialog) {
-                 $scope.closeDialog = function() {
-                    $mdDialog.hide();
-                 }
-              }
-           });
-        };
+		var originatorEv;
+		this.openMenu = function($mdOpenMenu, ev) {
+			originatorEv = ev;
+            $mdOpenMenu(ev);
+            };
 	}])	
 
 
