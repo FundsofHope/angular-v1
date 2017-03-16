28dd70b2c05140a54c21e0c2f58aa400247057da
nikhilsangwan007 <nikhil.sangwan007@gmail.com>
2017-03-17 04:33:18 +0530

    path correction

--- scripts/directives/allDirectives.js
+++ scripts/directives/allDirectives.js
@@ -3,13 +3,13 @@ angular.module('allDirectives', [])
 	.directive('toolBar', function() {
 		return {
 			restrict: 'E',
-			templateUrl: '../templates/directive-templates/tool-bar.html'
+			templateUrl: './templates/directive-templates/tool-bar.html'
 		};
 	})
 
 	.directive('sideNav', function() {
 		return {
 			restrict: 'E',
-			templateUrl: '../templates/directive-templates/side-nav.html'
+			templateUrl: './templates/directive-templates/side-nav.html'
 		};
 	})
